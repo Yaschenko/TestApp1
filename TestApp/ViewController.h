e32be9fc581d3b8637c565b0135fcd90172eb19d
Yura Yaschenko <yura.yaschenko@rokolabs.com>
2016-03-29 16:12:38 +0300

    Initial Commit

---
+++ TestApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by Yurii on 3/29/16.
+//  Copyright © 2016 Nostris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TestApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Yurii on 3/29/16.
+//  Copyright © 2016 Nostris. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
